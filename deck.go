f8509263faa19e896beef1ba2dd61335f62b8490
Hunter Laine <hunterglaine@gmail.com>
2021-06-04 14:52:50 -0700

    Create saveToFile function which created new text file

--- deck.go
+++ deck.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"strings"
 )
 
@@ -36,4 +37,9 @@ func (d deck) print() {
 
 func (d deck) toString() string {
 	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
+	// 0666 means anyone can read/write this file
 }
