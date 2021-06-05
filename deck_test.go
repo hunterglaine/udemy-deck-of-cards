a14609d9688e62690c41c6adc58762fefc2b3d58
Hunter Laine <hunterglaine@gmail.com>
2021-06-04 17:32:22 -0700

    Update test

--- deck_test.go
+++ deck_test.go
@@ -7,9 +7,11 @@ func TestNewDeck(t *testing.T) {
 	d := newDeck()
 	if len(d) != 52 {
 		t.Errorf("Expected deck length of 52, but got %v", len(d))
-	} else if d[0] != "Ace of Spades" {
+	}
+	if d[0] != "Ace of Spades" {
 		t.Errorf("Expected Ace of Spades, but got %v", d[0])
-	} else if d[len(d)-1] != "King of Clubs" {
+	}
+	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected Four of Clubs, but got %v", d[len(d)-1])
 	}
 }
